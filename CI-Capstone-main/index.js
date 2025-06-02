4df64b29c21a2e07332358cbc4d80cfd9a75bc24
Lorenzo <renzsantinot@gmail.com>
2025-06-02 19:24:23 -0400

    Both new and old versons of Weather App

---
+++ CI-Capstone-main/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the 'public' and 'images' directories
+app.use(express.static('public'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Function to fetch weather data
+const getWeatherData = async (city, units = 'metric') => {
+  const apiKey = '685e795d173e00ace5b19b16d2de5206';
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  const response = await axios.get(url);
+  return response.data;
+};
+
+app.get('/weather', async (req, res) => {
+  const city = req.query.city;
+  const units = req.query.units || 'metric'; // Default to metric if units are not specified
+
+  try {
+    const data = await getWeatherData(city, units);
+    res.json(data);
+  } catch (error) {
+    res.status(500).send('Error fetching weather data');
+  }
+});
+
+app.get('/constant-weather', async (req, res) => {
+  try {
+    const tokyoWeather = await getWeatherData('Tokyo');
+    const newYorkWeather = await getWeatherData('New York');
+    const londonWeather = await getWeatherData('London');
+    const sydneyWeather = await getWeatherData('Sydney');
+	const parisWeather = await getWeatherData('Paris');
+	const dubaiWeather = await getWeatherData('Dubai');
+
+    res.json({
+      tokyo: tokyoWeather,
+      newYork: newYorkWeather,
+      london: londonWeather,
+      sydney: sydneyWeather,s
+	  paris: parisWeather,
+	  dubai: dubaiWeather,
+    });
+  } catch (error) {
+    res.status(500).send('Error fetching constant weather data');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
